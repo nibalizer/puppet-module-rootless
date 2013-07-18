3ef0b0a06ffce40299d93418a016dc6985d9b018
Spencer Krum <skrum@go2uti.com>
2013-07-17 17:13:55 -0700

    updating usage on jdk

--- manifests/jdk.pp
+++ manifests/jdk.pp
@@ -6,31 +6,33 @@
 
 # Usage:
 
-# rootless::jdk { '/opt/app/place/':
-#   ensure              => present,
-#   jdk_major_version  => '7',
-#   jdk_update_version => '25',
-#   tarball_directory   => '/big/nfs/mount',
-# }
+#rootless::jdk { '/opt/app/place/':
+#  ensure              => present,
+#  jdk_major_version  => '7',
+#  jdk_update_version => '25',
+#  tarball_directory   => '/big/nfs/mount',
+#}
+#
+#rootless::jdk { '/opt/app/other_place/':
+#  ensure            => present,
+#  tarball_directory => '/big/nfs/mount',
+#  jdk_file         => 'my-special-jdk-tarball.tar.gz',
+#  jdk_install_dir  => 'jdk1.7.0_09',    # the directory created by the untar operation
+#}
+#
+#rootless::jdk { '/opt/app/yet_another_place/':
+#  ensure           => absent,
+#  jdk_install_dir => 'jdk-1.7.0_12',
+#}
+#
+#rootless::jdk { 'my-jdk-install':
+#  ensure              => present,
+#  jdk_major_version  => '7',
+#  jdk_update_version => '25',
+#  tarball_directory   => '/big/nfs/mount',
+#  install_root        => '/opt/app/best_place',
+#}
 
-# rootless::jdk { '/opt/app/other_place/':
-#   ensure            => present,
-#   tarball_directory => '/big/nfs/mount',
-#   jdk_file         => 'my-special-jdk-tarball.tar.gz',
-#   jdk_create_dir   => 'jdk-1.7.0_09',    # the directory created by the untar operation
-# }
-
-# rootless::jdk { '/opt/app/yet_another_place/':
-#   ensure            => absent,
-# }
-
-# rootless::jdk { 'my-jdk-install':
-#   ensure              => present,
-#   jdk_major_version  => '7',
-#   jdk_update_version => '25',
-#   tarball_directory   => '/big/nfs/mount',
-#   install_root        => '/opt/app/best_place',
-# }
 define rootless::jdk (
   $ensure,
   $tarball_directory = '',
@@ -38,7 +40,7 @@ define rootless::jdk (
   $jdk_update_version = '',
   $jdk_file = '',
   $jdk_install_dir= '',
-  $install_root = $name
+ $install_root = $name
 ){
 
   case $ensure {
